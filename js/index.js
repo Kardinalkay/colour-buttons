c7c59a85a1dd662c411492190484ad2efd5dcfef
Olagoke Kay <60120856+Kardinalkay@users.noreply.github.com>
2020-03-23 15:49:34 +0000

    Enable animation on colour change (#5)
    * Cache selectors
    
    * Add ids to buttons
    
    * Set the tone for background colour animation by setting specific classes on the body that would be activated by scripting.
    
    * Listen for click event on button and set corresponding class to body then delegate animation task to CSS by having already set a transition targeting the background property, on the body element
    
    * Increase transtion time from .3s to 1s

--- js/index.js
+++ js/index.js
@@ -1,13 +1,28 @@
 	
-	// Background toggle feature
+	// BACKGROUND COLOUR FEATURE
 
-	let colours = ['rgba(246, 34, 23, .5)', 'rgba(52, 124, 44, .5)', 'rgba(80, 235, 236, .5)'];
+	//let colours = ['rgba(246, 34, 23, .5)', 'rgba(52, 124, 44, .5)', 'rgba(80, 235, 236, .5)'];
+
+	let colours = ['red', 'green', 'blue'];
+
+	let $redBtn = document.querySelector('#red-btn'),
+		$greenBtn = document.querySelector('#green-btn'),
+		$blueBtn = document.querySelector('#blue-btn');
+
+	console.log('Green Button: %s', $greenBtn);
 
 	let el = document.querySelectorAll('.toggle-bg-color button');
 
 	for (let i=0; i < el.length; i++) {
+
+		// Listen to click on either of 3 buttons : 
 	    el[i].addEventListener ('click', function () {
-	        // let className = this.className;
-	        document.querySelector("body").style.background = colours[i];
+
+	    	// First remove background colour set on body 
+	    	document.querySelector("body").classList.remove('red', 'green', 'blue');
+
+	        // Then now set background colour to the body
+	        document.querySelector("body").classList.add(colours[i]);
+	        console.log(el);
 	    }, false);
 	}
